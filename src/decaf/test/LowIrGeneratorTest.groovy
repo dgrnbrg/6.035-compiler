6ea0a9c0e6e0c080e07151aee8cf855a7eb8e770
David S Greenberg <dgrnbrg@mit.edu>
2011-03-07 17:37:10 -0500

    Added LowIrGeneratorTest

---
+++ src/decaf/test/LowIrGeneratorTest.groovy
@@ -0,0 +1,41 @@
+package decaf.test
+import decaf.*
+import static decaf.BinOpType.*
+
+class LowIrGeneratorTest extends GroovyTestCase {
+  void testAssignment() {
+    def gen = new LowIrGenerator()
+    def hb = new HiIrBuilder()
+    def assignment = hb.Block{
+      var(name:'a', type:Type.INT)
+      Assignment {
+        Location('a')
+        BinOp(ADD) {
+          lit(3)
+          BinOp(MUL) {
+            lit(4)
+            lit(1)
+          }
+        }
+      }
+    }
+    gen.handleStatement(assignment)
+  }
+  void testIfElse() {
+    def gen = new LowIrGenerator()
+    def hb = new HiIrBuilder()
+    def if1 = hb.Block{
+      var(name:'a', type:Type.INT)
+      IfThenElse {
+        lit(true)
+        Block {
+          Assignment{ Location('a'); lit(2) }
+        }
+        Block {
+          Assignment{ Location('a'); lit(4) }
+        }
+      }
+    }
+    gen.handleStatement(if1)
+  }
+}
