2c7544c3b4867453c3655d9bb9e32e33db7c59ca
David S Greenberg <dgrnbrg@mit.edu>
2011-03-07 17:36:54 -0500

    Added graph traverser algorithm

---
+++ src/decaf/graph/Traverse.groovy
@@ -0,0 +1,44 @@
+package decaf.graph
+
+class TraverserEdge {
+  GraphNode src, dst
+
+  boolean equals(Object o) {
+    if (o instanceof TraverserEdge) {
+      return o.src.is(src) && o.dst.is(dst)
+    }
+    return false
+  }
+
+  int hashCode() {
+    return src.hashCode() + 17*dst.hashCode()
+  }
+}
+
+abstract class Traverser {
+  Set<GraphNode> visited = new HashSet<GraphNode>()
+
+  Set<TraverserEdge> edges = new HashSet<TraverserEdge>()
+
+  abstract void visitNode(GraphNode n);
+  abstract void link(GraphNode src, GraphNode dst);
+
+  void reset() {
+    visited.clear()
+    edges.clear()
+  }
+
+  void traverse(GraphNode start) {
+    def cur = start
+    def queue = []
+    while (cur != null) {
+      visitNode(cur)
+      visited.add(cur)
+      def intermediate = (cur.successors - visited)
+      queue += intermediate
+      edges.addAll(cur.successors.collect{new TraverserEdge(src:cur, dst:it)})
+      cur = queue ? queue.pop() : null
+    }
+    edges.each{ link(it.src, it.dst) }
+  }
+}
