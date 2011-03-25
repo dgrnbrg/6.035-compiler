a07978e8305706c7125575d44933cf763f1b2ce4
Nathan Rittenhouse <nathan@nox-eci.com>
2011-03-25 01:44:54 -0400

    Fixed typo: LowIrCallout -> LowIrCallOut

--- src/decaf/Dataflow.java
+++ src/decaf/Dataflow.java
@@ -6,42 +6,42 @@ interface JoinOperator<D>{
     public Set<D> joinOp(Set<Set<D>> inputs);
 }
 
-abstract class TransitionFunction<D>{
-    // By default, transition functions are the identity function.
-    // Simply extend this class and override the transition methods
+abstract class TransferFunction<D>{
+    // By default, transfer functions are the identity function.
+    // Simply extend this class and override the transfer methods
     // for the node types that you want to perform analysis on.
-    public Set<D> transition(LowIrValueNode node, Set<D> input){
+    public Set<D> transfer(LowIrValueNode node, Set<D> input){
 	return input;
     }
     
-    public Set<D> transition(LowIrIntLiteral node, Set<D> input){
+    public Set<D> transfer(LowIrIntLiteral node, Set<D> input){
 	return input;
     }
 
-    public Set<D> transition(LowIrMov node, Set<D> input){
+    public Set<D> transfer(LowIrMov node, Set<D> input){
 	return input;
     }
     
-    public Set<D> transition(LowIrBinOp node, Set<D> input){
+    public Set<D> transfer(LowIrBinOp node, Set<D> input){
 	return input;
     }
     
-    public Set<D> transition(LowIrReturn node, Set<D> input){
+    public Set<D> transfer(LowIrReturn node, Set<D> input){
 	return input;
     }
 
-    public Set<D> transition(LowIrStringLiteral node, Set<D> input){
+    public Set<D> transfer(LowIrStringLiteral node, Set<D> input){
 	return input;
     }
-    public Set<D> transition(LowIrMethodCall  node, Set<D> input){
-	return input;
-    }
-    
-    public Set<D> transition(LowIrCallout  node, Set<D> input){
+    public Set<D> transfer(LowIrMethodCall  node, Set<D> input){
 	return input;
     }
     
-    public Set<D> transition(LowIrCondJump  node, Set<D> input){
+    public Set<D> transfer(LowIrCallOut  node, Set<D> input){
+	return input;
+    }
+    
+    public Set<D> transfer(LowIrCondJump  node, Set<D> input){
 	return input;
     }
 }
@@ -51,11 +51,13 @@ class DataFlowAnalysis{
     private TransitionFunction transition;
     private LowIrNode start;
     
-    DataFlowAnalysis(JoinOperator join, TransitionFunction transition, LowIrNode start){
+    DataFlowAnalysis(JoinOperator join, TransitionFunction transition){
 	this.join = join;
 	this.transition = transition;
-	this.start = start;
     }
 
-    
+    public void run(LowIrNode start){
+	this.start = start;
+	System.out.println(">>", start);
+    }
 }
