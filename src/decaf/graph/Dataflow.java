dd9a48d775034e2488e49c0283bc32c070c43a97
Nathan Rittenhouse <nathan@nox-eci.com>
2011-03-28 20:12:08 -0400

    Lessened the verbosity of the DataFlowAnalysis class. Note that I have no way of testing this.

--- src/decaf/graph/Dataflow.java
+++ src/decaf/graph/Dataflow.java
@@ -3,166 +3,85 @@ import decaf.graph.*;
 import java.util.*;
 import org.apache.commons.collections.iterators.IteratorChain;
 
-abstract class Analysis<D>{
-    // The join operator
-    abstract public D joinOp(Set<D> inputs);
+// abstract class Analysis<D>{
+//     // The join operator
+//     abstract public D joinOp(Set<D> inputs);
     
-    // By default, transfer functions are the identity function.
-    // Simply extend this class and override the transfer methods
-    // for the node types that you want to perform analysis on.
-    public D transfer(LowIrNode node, D input){
-	return input;
-    }
+//     // By default, transfer functions are the identity function.
+//     // Simply extend this class and override the transfer methods
+//     // for the node types that you want to perform analysis on.
+//     public D transfer(LowIrNode node, D input){
+// 	return input;
+//     }
     
-    public D transfer(LowIrValueNode node, D input){
-	return input;
-    }
+//     public D transfer(LowIrValueNode node, D input){
+// 	return input;
+//     }
     
-    public D transfer(LowIrIntLiteral node, D input){
-	return input;
-    }
+//     public D transfer(LowIrIntLiteral node, D input){
+// 	return input;
+//     }
 
-    public D transfer(LowIrMov node, D input){
-	return input;
-    }
+//     public D transfer(LowIrMov node, D input){
+// 	return input;
+//     }
     
-    public D transfer(LowIrBinOp node, D input){
-	return input;
-    }
+//     public D transfer(LowIrBinOp node, D input){
+// 	return input;
+//     }
     
-    public D transfer(LowIrReturn node, D input){
-	return input;
-    }
+//     public D transfer(LowIrReturn node, D input){
+// 	return input;
+//     }
 
-    public D transfer(LowIrStringLiteral node, D input){
-	return input;
-    }
+//     public D transfer(LowIrStringLiteral node, D input){
+// 	return input;
+//     }
     
-    public D transfer(LowIrMethodCall  node, D input){
-	return input;
-    }
+//     public D transfer(LowIrMethodCall  node, D input){
+// 	return input;
+//     }
     
-    public D transfer(LowIrCallOut  node, D input){
-	return input;
-    }
+//     public D transfer(LowIrCallOut  node, D input){
+// 	return input;
+//     }
     
-    public D transfer(LowIrCondJump  node, D input){
-	return input;
-    }
+//     public D transfer(LowIrCondJump  node, D input){
+// 	return input;
+//     }
 
-    // Returns 'new D'
-    abstract public D emptyState();
-}
+//     // Returns 'new D'
+//     abstract public D emptyState();
+// }
 
-class DataFlowAnalysis<D>{
-    private Analysis analysis;
-    // private LowIrNode start;
+abstract class DataFlowAnalysis{
+    // transfer(node) returns true if the analysis data associated
+    // with node changes as a side effect of the transfer function.
+    abstract public boolean transfer(GraphNode node);
+    abstract public void storeOnNode(GraphNode node, Set data);
 
-    // Graph traversal bookkeeping
-    // private Set<LowIrNode> nodesVisited;
-    // private Set<TraverserEdge> edgesVisited;
-    private Map<GraphNode,D> input;
+    // Invariant: loadFromNode should return the empty set should
+    // it be ran against an un-annotated node.
+    abstract public Set loadFromNode(GraphNode node);
+    abstract public Set join(GraphNode node);
 
-    private boolean analysisTerminated;
-    
-    DataFlowAnalysis(Analysis<D> analysis){
-	this.analysis = analysis;
-	this.analysisTerminated = false;
-    }
+    private void analyze(GraphNode node){
+	// For forward dataflow analyses
+	Queue<GraphNode> workQueue = new LinkedList<GraphNode>();
 
-    // DataFlowAnalysis(){
-    // 	this.join = null;
-    // 	this.transfer = null;
-    // }
+	workQueue.offer(node);
 
-    private void initializeNode(GraphNode node){
-        if(!this.input.containsKey(node)){
-	    D inputState = (D)this.analysis.emptyState();
-	    // System.out.print("inputState:");
-	    // System.out.println(inputState);
-	    this.input.put(node, inputState);
-	}
-    }
-    
-    private D getNodeInput(GraphNode node){
-	assert(this.analysisTerminated == true);
-        D inputState = (D)this.input.get(node);
-	return inputState;
-    }
-
-    public D getNodeResult(GraphNode node){
-	return this.getNodeInput(node);
-    }
-
-    private D dispatchTransfer(GraphNode node){
-	// Get the set of inputs to be sent to the join operator
-	Set<D> inputs = new HashSet<D>();
-	for(GraphNode predecessor : node.getPredecessors()){
-	    inputs.add(this.getNodeInput(predecessor));
-	}
-	    
-	D inputState = (D)this.analysis.joinOp(inputs);
-	    
-	D transferOutput = null;
-	if(node instanceof LowIrValueNode){
-	    transferOutput = (D)this.analysis.transfer((LowIrValueNode)node, inputState);
-	}
-	else if(node instanceof LowIrIntLiteral){
-	    transferOutput = (D)this.analysis.transfer((LowIrIntLiteral)node, inputState);
-	}
-	else if(node instanceof LowIrMov){
-	    transferOutput = (D)this.analysis.transfer((LowIrMov)node, inputState);
-	}
-	else if(node instanceof LowIrBinOp){
-	    transferOutput = (D)this.analysis.transfer((LowIrBinOp)node, inputState);
-	}
-	else if(node instanceof LowIrReturn){
-	    transferOutput = (D)this.analysis.transfer((LowIrReturn)node, inputState);
-	}
-	else if(node instanceof LowIrStringLiteral){
-	    transferOutput = (D)this.analysis.transfer((LowIrStringLiteral)node, inputState);
-	}
-	else if(node instanceof LowIrMethodCall){
-	    transferOutput = (D)this.analysis.transfer((LowIrMethodCall)node, inputState);
-	}
-	else if(node instanceof LowIrCallOut){
-	    transferOutput = (D)this.analysis.transfer((LowIrCallOut)node, inputState);
-	}
-	else if(node instanceof LowIrCondJump){
-	    transferOutput = (D)this.analysis.transfer((LowIrCondJump)node, inputState);
-	}
-	else if(node instanceof LowIrNode){
-	    transferOutput = (D)this.analysis.transfer((LowIrNode)node, inputState);
-	}
-	System.out.println(transferOutput);
-	
-	return transferOutput;
-    }
-
-    private LinkedList<GraphNode> initializeNodes(GraphNode begin){
-	LinkedList<GraphNode> nodes = new LinkedList<GraphNode>();
-	Queue<GraphNode> workQueue = new LinkedList<GraphNode>();	
-	workQueue.offer(begin);
-
-	// First run of the analysis processes every node unconditionally
-	while(workQueue.peek() != null){
-	    GraphNode current = workQueue.poll();
-	    this.initializeNode(current);
-	    nodes.addLast(current);
-
-	    for(GraphNode successor : current.getSuccessors()){
-		workQueue.offer(successor);
+	while(workQueue != null){
+	    GraphNode = workQueue.poll();
+	    // If analysis yields different result from before, add
+	    // all successor nodes to be processed.
+	    if(this.transfer(node) == true){
+		for(GraphNode sucessor : node.getSuccessors()){
+		    workQueue.offer(successor);
+		}
 	    }
-	    
-	    // System.out.print(">> visited:");
-	    // System.out.println(current);
-	    // System.out.print("**:");
 	}
-	
-	return nodes;
     }
-
-    
 
     public void traverse(GraphNode begin){
 	// traverse() actually runs the dataflow functions against
@@ -209,13 +128,5 @@ class DataFlowAnalysis<D>{
 	    }
 	}
     }
-
-    public void run(LowIrNode start){
-	// this.start = start;
-	this.input = new HashMap<GraphNode,D>();
-
-	this.analysisTerminated = false;
-	this.traverse((GraphNode)start);
-	this.analysisTerminated = true;
-    }
 }
+
