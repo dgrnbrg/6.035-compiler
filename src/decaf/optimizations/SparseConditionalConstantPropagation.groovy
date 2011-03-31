9e0c678002ddf2c8ff75f84d0c11a1f04fa7fc64
Aysylu <aysylu7@gmail.com>
2011-03-31 19:44:11 -0400

    added a few test cases, added support for all binops for SCCP

--- src/decaf/optimizations/SparseConditionalConstantPropagation.groovy
+++ src/decaf/optimizations/SparseConditionalConstantPropagation.groovy
@@ -6,31 +6,92 @@ class SparseConditionalConstantPropagation {
 
   // contains only tmpVars that are either constant or overdefined
   def tmpToInstVal = [:] //keys:tmpVars, value: InstValues
+  HashSet visited
+  //HashSet<LowIrNode> controlWL
+  HashSet ssaWL
 
   //symbolic execution to simulate arithmetic
   def sccpEval(LowIrNode node) {
     //def valueChanged = false
+    def result = null
     switch (node) {
     case LowIrBinOp:
-      //def result = latticeCompute(node.getUses(), {list -> list[0] / list[1]})
-      if (node.op == BinOpType.NOT) {
-        def result = calculate(node, {x -> new InstVal(tmpToInstVal[node.leftTmpVar].constVal == 0 ? 1 : 0) })
-        tmpToInstVal[node] = result
-        println " the result.constVal is $result.constVal "
+      switch (node.op) {
+      case BinOpType.NOT: 
+        result = calculate(node, {x -> 
+          new InstVal(x == 0 ? 1 : 0) })
+        break
+      case BinOpType.ADD:
+        result = calculate(node, {x, y -> new InstVal(x + y) })
+        break
+      case BinOpType.SUB: 
+        result = calculate(node, {x, y -> new InstVal(x - y) })
+        break
+      case BinOpType.MUL:
+        result = calculate(node, {x, y -> new InstVal(x * y) })
+        break
+      case BinOpType.DIV:
+        result = calculate(node, {x, y -> new InstVal(x / y) })
+        break
+      case BinOpType.MOD:
+        result = calculate(node, {x, y -> new InstVal(x % y) })
+        break
+      case BinOpType.LT:
+        result = calculate(node, {x,y -> new InstVal( x - y < 0 ? 1 : 0) })
+        break
+      case BinOpType.GT:
+        result = calculate(node, {x,y -> new InstVal( x - y > 0 ? 1 : 0) })
+        break
+      case BinOpType.LTE:
+        result = calculate(node, {x,y -> new InstVal( x - y <= 0 ? 1 : 0) })
+        break
+      case BinOpType.GTE:
+        result = calculate(node, {x,y -> new InstVal( x - y >= 0 ? 1 : 0) })
+        break
+      case BinOpType.EQ:
+        result = calculate(node, {x,y -> new InstVal( x - y == 0 ? 1 : 0) })
+        break
+      case BinOpType.NEQ:
+        result = calculate(node, {x,y -> new InstVal( x - y != 0 ? 1 : 0) })
+        break
+      case BinOpType.AND:
+        result = calculate(node, {x,y -> new InstVal( x + y == 2 ? 1 : 0) })
+        break
+      case BinOpType.OR:
+        result = calculate(node, {x,y -> new InstVal( x + y >= 1  ? 1 : 0) })
+        break
       }
-
+    break
+    case LowIrPhi:
+      result = calculate(node, null)
+      break
     }
+    tmpToInstVal[node] = result
   }
 
   InstVal calculate(LowIrNode node, Closure resolve) {
-    if (node.getUses().any { tmpToInstVal[it].latticeVal == LatticeType.UNDEF }) {
-      return new InstVal(latticeVal: LatticeType.UNDEF)
-    } else if (node.getUses().any {tmpToInstVal[it].latticeVal == LatticeType.OVERDEF }) {
-      return new InstVal(latticeVal: LatticeType.OVERDEF)
+    switch (node) {
+    case LowIrBinOp:
+      if (node.getUses().any { tmpToInstVal[it].latticeVal == LatticeType.UNDEF }) {
+        return new InstVal(LatticeType.UNDEF)
+      } else if (node.getUses().any {tmpToInstVal[it].latticeVal == LatticeType.OVERDEF }) {
+        return new InstVal(LatticeType.OVERDEF)
+      } else {
+        //all uses are constants
+        def args = node.getUses().collect { tmpToInstVal[it].constVal }
+        def instVal = resolve(*args)
+        return instVal
+      }
+    case LowIrPhi:
+      if (node.getUses().any { tmpToInstVal[it].latticeVal == LatticeType.OVERDEF}) {
+      return new InstVal(LatticeType.OVERDEF)
+    } else if (node.getUses().every { tmpToInstVal[it].latticeVal == LatticeType.UNDEF}) {
+      return new InstVal(LatticeType.UNDEF)
     } else {
-      //all uses are constants
-      def instVal = resolve.call(node.getUses().collect { tmpToInstVal[it].constVal })
-      return instVal
+      def listConsts = node.getUses().collect{ tmpToInstVal[it] }.findAll { it.latticeVal == LatticeType.CONST }*.constVal
+      assert listConsts.size() > 0
+      return listConsts.every { it == listConsts[0] } ? new InstVal(listConsts[0]) : InstVal(LatticeType.OVERDEF)
+    }
     }
   }
 }
@@ -45,9 +106,9 @@ class InstVal {
     this.latticeVal = latticeVal
   }
 
-  InstVal(int constVal) {
+  InstVal(Number constVal) {
     this.latticeVal = LatticeType.CONST
-    this.constVal = constVal
+    this.constVal = constVal as int
   }
 }
 
